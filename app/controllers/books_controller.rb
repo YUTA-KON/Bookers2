efb31dbbcb605f02a8786e31410c8dff2582e816
YUTA-KON <dorata.1007@gmail.com>
2021-06-20 12:00:15 +0900

    add users/index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,37 @@
 class BooksController < ApplicationController
-    def new
+    def edit
+    end
+
+    def update
     end
 
     #投稿データの保存
     def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        if @book.save
+            redirect_to books_path
+        else
+            render :new
+        end
     end
 
     def index
+        @user = current_user
+        @newbook = Book.new
+        @books = Book.all
     end
 
     def show
+        @book = Book.find(params[:id])
+        @user = @book.user
+        @newbook = Book.new
     end
 
     def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_path
     end
 
     # 投稿データのストロングパラメータ
